67892801f784ff379632d6fc346f82e6451476f0
AttigH <hamzaatttig@gmail.com>
2021-11-09 02:45:20 +0200

    redux product details

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,5 +1,9 @@
+import axios from "axios";
 import Axios from "axios";
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -19,6 +23,28 @@ export const listProduct = () => async (dispatch) => {
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload: error.message,
+    });
+  }
+};
+
+export const detailsProduct = (productId) => async (dispatch) => {
+  dispatch({
+    type: PRODUCT_DETAILS_REQUEST,
+    payload: productId,
+  });
+  try {
+    const { data } = await axios.get(`/api/product/${productId}`);
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
     });
   }
 };
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,67 +1,91 @@
-import React from "react";
-import data from "../data";
+import React, { useEffect } from "react";
+// import data from "../data";
 import Rating from "../components/Rating";
 import { useParams, Link } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { detailsProduct } from "../actions/productAction";
 export default function ProductScreen(props) {
+  // const params = useParams();
+  // console.log(params.id);
+  // const product = data.products.find((x) => x._id === params.id);
+  // if (!product) {
+  //   return <div> Product Not found </div>;
+  // }
   const params = useParams();
-  console.log(params.id);
-  const product = data.products.find((x) => x._id === params.id);
-  if (!product) {
-    return <div> Product Not found </div>;
-  }
+  const dispatch = useDispatch();
+  const productId = params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
   return (
     <div>
-      <Link to="/">Back to result</Link>
-      <div className="row top">
-        <div className="col-2">
-          <img className="large" src={product.image} alt={product.name}></img>
-        </div>
-        <div className="col-1">
-          <ul>
-            <li>
-              <h1>{product.name}</h1>
-            </li>
-            <li>
-              <Rating
-                rating={product.rating}
-                numReviews={product.numReviews}
-              ></Rating>
-            </li>
-            <li>Price : ${product.price}</li>
-            <li>
-              Description :<p>{product.description}</p>
-            </li>
-          </ul>
-        </div>
-        <div className="col-1">
-          <div className="card card-body">
-            <ul>
-              <li>
-                <div className="row">
-                  <div>Price</div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </li>
-              <li>
-                <div className="row">
-                  <div>Status</div>
-                  <div>
-                    {product.countInStock > 0 ? (
-                      <span className="success">In Stock</span>
-                    ) : (
-                      <span className="danger">Unavailable</span>
-                    )}
-                  </div>
-                </div>
-              </li>
-              <li>
-                <button className="primary block">Add to Cart</button>
-              </li>
-            </ul>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger"> {error} </MessageBox>
+      ) : (
+        <div>
+          <Link to="/">Back to result</Link>
+          <div className="row top">
+            <div className="col-2">
+              <img
+                className="large"
+                src={product.image}
+                alt={product.name}
+              ></img>
+            </div>
+            <div className="col-1">
+              <ul>
+                <li>
+                  <h1>{product.name}</h1>
+                </li>
+                <li>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  ></Rating>
+                </li>
+                <li>Price : ${product.price}</li>
+                <li>
+                  Description :<p>{product.description}</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-1">
+              <div className="card card-body">
+                <ul>
+                  <li>
+                    <div className="row">
+                      <div>Price</div>
+                      <div className="price">${product.price}</div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="row">
+                      <div>Status</div>
+                      <div>
+                        {product.countInStock > 0 ? (
+                          <span className="success">In Stock</span>
+                        ) : (
+                          <span className="danger">Unavailable</span>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <button className="primary block">Add to Cart</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
