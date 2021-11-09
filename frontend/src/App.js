dc83b15fa827fea9cc9ce6304c92f8dd08ca19c9
AttigH <hamzaatttig@gmail.com>
2021-11-09 17:09:34 +0200

    Add to Cart Actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./index.css";
 import ProductScreen from "./screens/ProductScreen";
 import HomeScreen from "./screens/HomeScreen";
 import CartScreen from "./screens/CartScreen";
+import { useSelector } from "react-redux";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="index.html">
+            <Link className="brand" to="/">
               Shopi
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a>
+            <Link to="cart.html">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge"> {cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="signin.html">Sign In</Link>
           </div>
         </header>
 
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
-
+import { addToCart } from "../actions/cartActions";
+import { useDispatch } from "react-redux";
 const CartScreen = (props) => {
   const params = useParams();
   const location = useLocation();
+  const dispatch = useDispatch();
   const productId = params.id;
   const qty = location.search ? Number(location.search.split("=")[1]) : 1;
-  console.log(productId);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
   return (
     <div>
       <h1> Cart Screen</h1>
